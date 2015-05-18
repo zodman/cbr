958888b3ca415e8713a143034de3eb735773f77f
Andres Vargas <andres.vargas@interalia.net>
2015-05-18 18:18:14 -0500

    upadte csv

--- mercadolibre_kb.py
+++ mercadolibre_kb.py
@@ -17,22 +17,24 @@ URL = "http://waifu.ca/"
 TOKEN = os.environ.get("TOKEN")
 auth_params = dict(client_id=client_id, client_secret=client_secret)
 SITE = "MLM"
-TIMES  = random.randint(1,5)
+TIMES  = random.randint(1,2)
 NUM_PAGE = random.randint(1,10)
-LIMIT = random.randint(1,20)
+LIMIT = 30 # random.randint(1,10)
 FROM_DATE = datetime.datetime(year=2000, month=5, day=24)
 TO_DATE = datetime.datetime.now()
 
 def write_cvs(data):
-    f = open(sys.argv[1], "w")
-    wr = csv.writer(f, encoding="utf-8", quoting=csv.QUOTE_ALL)
+    f_ = open(sys.argv[1], "w")
+    wr = csv.writer(f_, encoding="utf-8", quoting=csv.QUOTE_ALL)
     for d in data:
-        for i in d["items"]:
+        items = d["items"]
+        for i in [dict(t) for t in set([tuple(f.items()) for f in items])]:
             random_date = radar.random_datetime(start=FROM_DATE, stop=TO_DATE)
             date_ = random_date.strftime("%x")
             time_ = random_date.strftime("%X")
-            wr.writerow((i["item_name"], d['cat_name'], i['item_price'], date_, time_, i['item_transaction']))
-    f.close()
+            tran = random.randint(0,1)
+            wr.writerow((i["item_name"], d['cat_name'], i['item_price'], date_, time_, tran))
+    f_.close()
 
 def main():
     if not  TOKEN:
@@ -61,8 +63,8 @@ def main():
         cat_name = cat.get("name")
         d = {'cat_id':cat_id, 'cat_name':cat_name, 'items':[]}
         cat_count +=1
+        print d
         for page in range(0,TIMES):
-            page = random.randint(1,NUM_PAGE)
             url = "/sites/{}/hot_items/search?limit={}&category={}&page={}".format(SITE,LIMIT,cat_id, page)
             resp = ml_api.get(url)
             item_list = resp.json()["results"]
@@ -72,9 +74,9 @@ def main():
                 item_id = item.get("id")
                 item_price = item.get("price")
                 if item_name:
-                    tran = random.randint(0,1)
                     d["items"].append({'item_id': item_id,
-                        'item_price':item_price, 'item_name': item_name, 'item_transaction':tran})
+                        'item_price':item_price, 'item_name': item_name})
+                    #print d
         data.append(d)
     print "fetch"
     print "cats: {} items: {}".format(cat_count, item_count)
